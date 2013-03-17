c6bde6a37a924f0c36cdf4c1bdc619fc21eb3659
cyprieng <cyprien.guillemot@gmail.com>
2013-03-17 16:35:27 +0100

    Solve encodings issues in the media library

--- modeles/home.php
+++ modeles/home.php
@@ -7,7 +7,7 @@
 	*/
 	function getArborescence($folder, $extension){
 		$pointeur = opendir($folder); //on ouvre un pointeur sur le repertoire
-		$arborescence[] = utf8_encode($folder); //Première case => nom dossier
+		$arborescence[] = (mb_detect_encoding($folder, "UTF-8,ISO-8859-1,WINDOWS-1252") == 'UTF-8') ? $folder:utf8_encode($folder); //Première case => nom dossier
 
 		//Récupération dossier dans l'ordre alphabétique
 		$sub_folder = scandir($folder, 2);
@@ -18,7 +18,7 @@
 					$arborescence[] = getArborescence($folder.'/'.$file, $extension);
 				}
 				else if(in_array(substr(strrchr($file,'.'),1), $extension)){ //Si fichier on vérifit l'extension
-					$arborescence[] = utf8_encode($folder.'/'.$file);
+					$arborescence[] = (mb_detect_encoding($folder.'/'.$file, "UTF-8,ISO-8859-1,WINDOWS-1252") == 'UTF-8') ? $folder.'/'.$file:utf8_encode($folder.'/'.$file);
 				}
 			}
 		}
